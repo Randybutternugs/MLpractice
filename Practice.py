516258030f1e7706a8a11ddc02994d44092ee006
Randybutternugs <adonisalexis@icloud.com>
2020-03-12 18:22:17 -0400

    Checkpoint

--- Practice.py
+++ Practice.py
@@ -21,10 +21,10 @@ print(t)
 
 #-=-=-=-=-=END RESHAPING=-=-=-=-=-
 
+
 ##################################################
-############ MOD 3 ###############################
 ############ CORE LEARNING ALGORITHMS ############
-##################################################
+############## Linear regression EX ##############
 from __future__ import absolute_import, division, print_function, unicode_literals
 import numpy as np
 import pandas as pd
@@ -169,3 +169,81 @@ print(y_eval.loc[3])
 ############################################################################
 #################### CLASSIFICATION ########################################
 ############################################################################
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+import pandas as pd
+
+# This specific dataset seperates flowers into 3 different classes of species.
+
+# Setosa
+# Versicolor
+# Virginica
+
+# --The information about each flower is the following.
+
+# sepal length
+# sepal width
+# petal length
+# petal width
+
+CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
+SPECIES = ['Setosa', 'Versicolor', 'Virginica']
+# Lets define some constants to help us later on
+
+train_path = tf.keras.utils.get_file("iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv") # Saves file as iris_training.csv, gets it from link
+test_path = tf.keras.utils.get_file("iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
+
+#trains and tests into two different dataframes
+train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
+test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
+# Here we use keras (a module inside of TensorFlow) to grab our datasets and read them into a pandas dataframe
+
+#note all numbers are in cm
+train.head()
+
+train_y = train.pop('Species')
+test_y = test.pop('Species')
+train.head() # the species column is now gone
+
+train.shape
+#we have 120 entries with 4 features (columns)
+
+#Input function
+def input_fn(features, labels, training=True, batch_size=256):
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle and repeat if you are in training mode.
+    if training:
+        dataset = dataset.shuffle(1000).repeat()
+
+    return dataset.batch(batch_size)
+
+
+# Feature columns describe how to use the input.
+my_feature_columns = []
+for key in train.keys():
+    my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+print(my_feature_columns)
+
+# We use the DNN (Deep neural network) model because we may not be able to find a linear coorespondance in our data
+# Now we build the model
+# Build a DNN with 2 hidden layers with 30 and 10 hidden nodes each.
+classifier = tf.estimator.DNNClassifier(
+    feature_columns=my_feature_columns,
+    # Two hidden layers of 30 and 10 nodes respectively.
+    hidden_units=[30, 10],
+    # The model must choose between 3 classes.
+    n_classes=3)
+
+
+# Now we Train the model
+classifier.train(
+    input_fn=lambda: input_fn(train, train_y, training=True),
+    steps=5000)
+# We include a lambda to avoid creating an inner function previously
+# lambda allows for one line functions ex:
+# x = lambda: print('hi')
+# x()
+# That works
