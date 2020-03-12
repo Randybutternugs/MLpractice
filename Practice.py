dc4f424aa680bce846e25535c0e37873be5269a2
Randybutternugs <adonisalexis@icloud.com>
2020-03-12 16:35:06 -0400

    moving on to classification

--- Practice.py
+++ Practice.py
@@ -116,3 +116,56 @@ for feature_name in NUMERIC_COLUMNS:
 # every epoch is the number of times the model will see the whole dataset
 # the more times the model sees the entire dataset the better it will determine how to estimate it
 # so if we have 10 epochs the model will have seen the data 10 times
+# Sometimes you can overfit which is when the model knows a dataset TOO well because its seen it too often
+# This leads the model to do horribly when predicting new entries because its too familiar with the training data
+
+# We must create an input function that converts/encodes pandas dataframe data into usable tf.data.Dataset objects that our model can read
+# This also allows us to feed the data in batches
+# So in general an input function breaks data into epochs and batches to  be fed to our model
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():  # inner function, this will be returned
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
+    if shuffle:
+      ds = ds.shuffle(1000)  # randomize order of data
+    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
+    return ds  # return a batch of the dataset
+  return input_function  # return a function object for use
+
+train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+# CREATING THE MODEL
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+# We create a linear estimtor by passing the feature columns we created earlier
+# Now its alive, and is a linear estimator, but a blank slate
+
+
+# TRAINING THE MODEL
+linear_est.train(train_input_fn)  # train
+result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
+
+clear_output()  # clears console output
+print(result['accuracy'])  # the result variable is simply a dict of stats about our model
+print(result)
+
+
+# Now that we've trained our model we need to make an input function so that we can give it data and it will make predictions for us
+result = list(linear_est.predict(eval_input_fn))
+print(result[0])
+
+#look at the person its predicting on at index 3
+print(dfeval.loc[3])
+# This gives us the result of person at index 3 and their chance of survival
+print(result[3]['probabilities'][1])
+
+#this tells us if they actually survived or not
+print(y_eval.loc[3])
+
+#the model predicted a 53% chance of survival and the person in reality did survive
+
+
+
+############################################################################
+#################### CLASSIFICATION ########################################
+############################################################################
