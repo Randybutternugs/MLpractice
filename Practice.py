7051d143e321193635888d6b939b2de9b5f91ed8
Randybutternugs <adonisalexis@icloud.com>
2020-03-27 13:56:00 -0400

    fin markov

--- Practice.py
+++ Practice.py
@@ -310,3 +310,30 @@ for pred_dict in predictions:
 # states
 # Observation distribution
 # Transition distribution
+
+
+import tensorflow_probability as tfp
+import tensorflow as tf
+
+tfd = tfp.distributions #shortcut for later on
+initial_distribution = tfd.Categorical(probs = [0.8, 0.2])
+transition_distribution = tfd.Categorical(probs = [[0.7, 0.3],
+                                                   [0.2, 0.8]])
+observation_distribution = tfd.Normal(loc = [0., 15.], scale = [5., 10.])
+
+model = tfd.HiddenMarkovModel(
+    initial_distribution = initial_distribution,
+    transition_distribution = transition_distribution,
+    observation_distribution = observation_distribution,
+    num_steps = 7)
+
+mean = model.mean()
+
+# due to the way TensorFlow works on a lower level we need to evaluate part of the graph
+# from within a session to see the value of this tensor
+
+# in the new version of tensorflow we need to use tf.compat.v1.Session() rather than just tf.Session()
+
+with tf.compat.v1.Session() as sess:
+    print(mean.numpy())
+     
